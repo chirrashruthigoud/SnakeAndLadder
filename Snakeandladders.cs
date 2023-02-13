23caacd12996889aa1837d6925e2e4b896c1d2b4
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-13 22:09:21 +0530

    snake and ladder Player Poition

---
+++ Snakeandladders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Snakeandladders
+    {
+        public static void CheckSnakeandladders() {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+        }
+    }
+}
