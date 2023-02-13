fd9505a16a5d3a31c1b19390c6516865e604df66
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-13 22:00:45 +0530

    snake and ladder game

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+        {
+    public static void Main(String[] args)
+    {
+            Console.WriteLine("Snake & Ladder Simulator this problem simulates a Snake and Ladder Game");
+           // Snakeandladders.CheckSnakeandladders();
+
+        }
+    }
+
+}
