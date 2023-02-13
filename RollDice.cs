e5527fd6ea120cb8f5ae234941f626056dba6e8e
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-13 22:21:08 +0530

    snake ladder Player Roll Dice

---
+++ RollDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class RollDice
+    {
+        public static void RollDiceplayer()
+        {
+            Random random = new Random();
+            int player = random.Next(1, 7);
+            Console.WriteLine("Player Roll the Dice : " + player);
+        }
+    }
+}
