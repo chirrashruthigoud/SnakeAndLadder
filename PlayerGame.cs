f770792f097f40f109db72ff0ca039d2e7396e57
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-13 22:33:55 +0530

    snake ladder Player Position Game

---
+++ PlayerGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class PlayerGame
+    {
+        public static void GameplayerPosition()
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            int count = 0;
+            int lastPosition = 100;
+            Random random = new Random();
+            Random random1 = new Random();
+            for (int i = 0; i < lastPosition; i++)
+            {
+                while (count <= lastPosition)
+                {
+                    int result = random.Next(0, 3);
+                    int Dice = random1.Next(1, 7);
+                    if (position == 0)
+                    {
+                        if (result == 0)
+                        {
+                            Console.WriteLine("Snake");
+                            Console.WriteLine(" Player moves behind by the number of position received in the dice");
+                            position = position - Dice;
+                            Console.WriteLine("position moves back : " + position);
+                        }
+                        else if (result == 1)
+                        {
+                            Console.WriteLine("Ladder");
+                            Console.WriteLine(" Player moves ahead by the number of position received in the die");
+                            position = position + Dice;
+                            Console.WriteLine("position moves ahead : " + position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Play");
+                            Console.WriteLine("Player Will Stay in the same position");
+                        }
+                    }
+                }
+                count++;
+            }
+        }
+    }
+}
